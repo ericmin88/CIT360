21bcc05613b2e708857ecced08d71bf5811daa5a
ericmin88 <min15010@byui.edu>
2016-10-29 11:17:34 -0600

    Add files via upload

---
+++ Model.java
@@ -0,0 +1,22 @@
+package MVC;
+
+public class Model {
+	private String Number;
+	private String Name;
+	   
+	public String getNumber() {
+		return Number;
+	}
+	  
+	public void setNumber(String Number) {
+		this.Number = Number;
+	}
+	   
+	public String getName() {
+		return Name;
+	}
+	   
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+}
