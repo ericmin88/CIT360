ad0d4d5837bed0fa0086e1b5946da47dad857a44
ericmin88 <min15010@byui.edu>
2016-10-29 11:18:00 -0600

    Add files via upload

---
+++ View.java
@@ -0,0 +1,8 @@
+package MVC;
+
+public class View {
+	public void printStudentDetails(String PlayerName, String PlayerNumber){
+		System.out.println("Player " + PlayerNumber);
+		System.out.println(PlayerName);
+	}
+}
